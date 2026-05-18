583cc3dde4cba837b39406a9248370a67a722586
Ajdin Sahinbegovic <ajdin_sah@hotmail.com>
2026-05-18 21:06:04 +0200

    fix dockr grafana

--- grafana/docker-entrypoint.sh
+++ grafana/docker-entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 set -e
 
-# Set defaults if not provided
 export PROMETHEUS_URL="${PROMETHEUS_URL:-http://localhost:9090}"
 export GCP_PROJECT_ID="${GCP_PROJECT_ID:-unknown}"
 
-envsubst '${GCP_PROJECT_ID} ${PROMETHEUS_URL}' < /etc/grafana/provisioning/datasources/datasources.yml > /tmp/datasources.yml
-cp /tmp/datasources.yml /etc/grafana/provisioning/datasources/datasources.yml
+# All writes go under /tmp (the only writable filesystem on Cloud Run)
+mkdir -p /tmp/grafana-provisioning/datasources /tmp/grafana-provisioning/dashboards
+
+cp /etc/grafana/provisioning/dashboards/dashboards.yml \
+   /tmp/grafana-provisioning/dashboards/
+
+envsubst '${GCP_PROJECT_ID} ${PROMETHEUS_URL}' \
+  < /etc/grafana/provisioning/datasources/datasources.yml \
+  > /tmp/grafana-provisioning/datasources/datasources.yml
+
+export GF_PATHS_PROVISIONING=/tmp/grafana-provisioning
 
 exec /run.sh
